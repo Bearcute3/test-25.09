5a5bd1e322e3d9e7167aff8b6f8d552f5b509d89
Bearcute3 <35570449+Bearcute3@users.noreply.github.com>
2018-09-25 22:01:57 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,1 +1,20 @@
-
+ans = input()
+ans2 = ""
+count = 0
+count2 = 0
+i = len(ans) - 1
+while i != -1:
+    if ans[i] != " ":
+        count += 1
+    if ans[i] == " " or i == 0:
+        while count2 != count:
+            if i==0:
+                ans2 = ans2 + ans[i + count2]
+            else:
+                ans2 = ans2 + ans[i + 1 + count2]
+            count2 += 1
+        count = 0
+        count2 = 0
+        ans2 = ans2 + " "
+    i -= 1
+print(ans2)
